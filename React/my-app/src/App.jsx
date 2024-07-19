fd40d33e49f657acea92a1c56e6edf89a241c3a1
Nicolas Papappicco <nicolas.papappicco@gmail.com>
2024-07-19 23:00:19 +0200

    react app

---
+++ React/my-app/src/App.jsx
@@ -0,0 +1,10 @@
+import { BeautifulDay } from "./BeautifulDay";
+import { HelloWorld } from "./HelloWorld";
+
+export function App() {
+    return (
+        <div>
+            <HelloWorld/>
+        </div>
+    )
+}
